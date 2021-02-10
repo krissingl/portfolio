ab5026cb0c35633caaefa180b1cac0ace3cceb37
Krista Singleton <kris.singlet231@gmail.com>
2021-02-09 19:45:28 -0700

    Fixed store file with writing out a reducer function

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,16 @@
 import { createStore } from 'redux';
 
-const store = createStore();
+const initialState = {
+  color: 'blue',
+};
+
+const reducer = (state = initialState, action) => {
+  if (action.type === 'changeColor') {
+    return { ...state, color: action.payload };
+  }
+  return state;
+};
+
+const store = createStore(reducer);
 
 export default store;
