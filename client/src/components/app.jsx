0ed4be0e25b468ff993b0ccaf66440ebf271a29d
Krista Singleton <kris.singlet231@gmail.com>
2021-02-10 21:15:41 -0700

    Added classes import and basic style for testing

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Home from './home.jsx';
+import classes from '../css/styles.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       page: 'home',
+      portfolioStyle: 'bidnez',
     };
   }
 
   render() {
     let currentPage;
+    const currentStyle = classes[this.state.portfolioStyle];
     if (this.state.page === 'home') {
       currentPage = (
         <div>
@@ -19,7 +22,7 @@ class App extends React.Component {
       );
     }
     return (
-      <div>
+      <div className={currentStyle}>
         <h2>Portfolio App</h2>
         <button type="button">Toggle Style</button>
         <div>
