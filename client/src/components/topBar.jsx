9f6c83b40cbbd12cd53aab82fc632a50dc44c5fa
Krista Singleton <kris.singlet231@gmail.com>
2021-02-12 21:03:06 -0700

    Fixed up topbar and app.jsx

--- client/src/components/topBar.jsx
+++ client/src/components/topBar.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const TopBar = () => {
   console.log('topbar component fired');
   return (
-    <div>TopBarComponent</div>
+    <div>
+      <h1>Portfolio App</h1>
+      <h5>TooBar Component</h5>
+    </div>
   );
 };
 
