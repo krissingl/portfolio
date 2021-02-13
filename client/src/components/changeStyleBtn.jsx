1e5c511c11c510c88c1bb1131dfb6780c42791da
Krista Singleton <kris.singlet231@gmail.com>
2021-02-12 21:11:56 -0700

    Built skelton of ChangeStyle compoennt

--- client/src/components/changeStyleBtn.jsx
+++ client/src/components/changeStyleBtn.jsx
@@ -1,1 +1,12 @@
 import React from 'react';
+
+const ChangeStyle = () => {
+  console.log('Change style of app');
+  return (
+    <div>
+      <button type="button">Change Style</button>
+    </div>
+  );
+};
+
+export default ChangeStyle;
