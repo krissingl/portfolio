d2295c2d8526202d3029c9b9183cff57437b8bd9
Krista Singleton <kris.singlet231@gmail.com>
2021-02-12 21:15:15 -0700

    Imprted connect from react-redux and copy/pasted code from app.jsx to find opposite style

--- client/src/components/changeStyleBtn.jsx
+++ client/src/components/changeStyleBtn.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const ChangeStyle = () => {
+const ChangeStyle = ({ portfolioStyle }) => {
   console.log('Change style of app');
+  let oppositeStyle;
+  if (this.state.portfolioStyle === 'bidnez') {
+    oppositeStyle = 'Change to Fun Timez style';
+    currentClasses = classes1;
+  } else if (this.state.portfolioStyle === 'funtimez') {
+    oppositeStyle = 'Change to Strictly Business style';
+    currentClasses = classes2;
+  }
   return (
     <div>
       <button type="button">Change Style</button>
