b15a93e1c42779a737468f935ea4e86e0ddd998d
Krista Singleton <kris.singlet231@gmail.com>
2021-02-13 22:48:14 -0700

    Tried to resolve portfolioStyle state not saving to redux store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,12 +1,15 @@
 import { createStore } from 'redux';
 
 const initialState = {
+  page: 'home',
   portfolioStyle: 'bidnez',
 };
 
 const reducer = (state = initialState, action) => {
   if (action.type === 'changeStyle') {
     return { ...state, portfolioStyle: action.payload };
+  } if (action.type === 'changePage') {
+    return { ...state, page: action.payload };
   }
   return state;
 };
