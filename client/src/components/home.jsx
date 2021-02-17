b76f69a1d4b2b87665ec4c8ed1fb2858ac1185ff
Krista Singleton <kris.singlet231@gmail.com>
2021-02-16 19:11:22 -0700

    Cleaned up code on home component

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-const Home = () => {
-  console.log('This is the Home page');
-  return (
-    <div>
-      <h1>HOME PAGE COMPONENT</h1>
-    </div>
-  );
-};
+const Home = () => (
+  <div>
+    <h1>Welcome to the home page!</h1>
+  </div>
+);
 
 export default Home;
