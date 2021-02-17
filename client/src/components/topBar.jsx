1f15541a8ab991749a217e8200fa8cf572e8aa1b
Krista Singleton <kris.singlet231@gmail.com>
2021-02-16 19:26:20 -0700

    Cleaned up code in topBar component

--- client/src/components/topBar.jsx
+++ client/src/components/topBar.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import classesMain from '../css/styles_main.css';
 
 const TopBar = () => {
-  console.log('topbar component fired');
+  const iconArr = ['icon1', 'icon2', 'icon3'];
   return (
     <div className={classesMain.topBarBox}>
       <h1>Portfolio App</h1>
       <div className={classesMain.navBox}>
-        <h5 className={classesMain.navBoxIcons}>icon1</h5>
-        <h5 className={classesMain.navBoxIcons}>icon2</h5>
-        <h5 className={classesMain.navBoxIcons}>icon3</h5>
+        <h5 className={classesMain.navBoxIcons}>{iconArr[0]}</h5>
+        <h5 className={classesMain.navBoxIcons}>{iconArr[1]}</h5>
+        <h5 className={classesMain.navBoxIcons}>{iconArr[2]}</h5>
       </div>
     </div>
   );
