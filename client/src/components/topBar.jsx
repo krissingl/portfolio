fa69864ef0bf1e11b12e0a4434c534d08c91615a
Krista Singleton <kris.singlet231@gmail.com>
2021-02-16 19:38:57 -0700

    Added change page functionality

--- client/src/components/topBar.jsx
+++ client/src/components/topBar.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { connect } from 'react-redux';
 import classesMain from '../css/styles_main.css';
 
-const TopBar = () => {
-  const iconArr = ['icon1', 'icon2', 'icon3'];
+const TopBar = ({ dispatch }) => {
+  const changePage = (page) => {
+    dispatch({
+      type: 'changePage',
+      payload: page,
+    });
+  };
   return (
     <div className={classesMain.topBarBox}>
       <h1>Portfolio App</h1>
       <div className={classesMain.navBox}>
-        <button type="button" className={classesMain.navBoxIcons}>HOME</button>
-        <button type="button" className={classesMain.navBoxIcons}>PICTURES</button>
-        <button type="button" className={classesMain.navBoxIcons}>THIRD THING</button>
+        <button type="button" className={classesMain.navBoxIcons} onClick={() => { changePage('home'); }}>HOME</button>
+        <button type="button" className={classesMain.navBoxIcons} onClick={() => { changePage('picPage'); }}>PICTURES</button>
+        <button type="button" className={classesMain.navBoxIcons} onClick={() => { changePage('home'); }}>THIRD THING</button>
       </div>
     </div>
   );
