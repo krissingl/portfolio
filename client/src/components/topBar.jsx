11eb4c2c978dd42c297fd1d468b015a8c8e76e0a
Krista Singleton <kris.singlet231@gmail.com>
2021-02-16 19:17:18 -0700

    Began adding styles to topbar component

--- client/src/components/topBar.jsx
+++ client/src/components/topBar.jsx
@@ -4,9 +4,11 @@ import classesMain from '../css/styles_main.css';
 const TopBar = () => {
   console.log('topbar component fired');
   return (
-    <div >
+    <div className={classesMain.topBarBox}>
       <h1>Portfolio App</h1>
-      <h5>TopBar Component</h5>
+      <div>
+        <h5>TopBar Component</h5>
+      </div>
     </div>
   );
 };
