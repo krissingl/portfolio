23f1e7e43db942b4fa49c8e1f4334b5608d0a7b3
Krista Singleton <kris.singlet231@gmail.com>
2021-02-16 19:14:37 -0700

    Added a main style css file and imported it to topBar compoennt

--- client/src/components/topBar.jsx
+++ client/src/components/topBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import classesMain from '../css/styles_main.css';
 
 const TopBar = () => {
   console.log('topbar component fired');
   return (
-    <div>
+    <div >
       <h1>Portfolio App</h1>
       <h5>TopBar Component</h5>
     </div>
