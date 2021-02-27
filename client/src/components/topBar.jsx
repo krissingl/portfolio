de7bc51cca2dc9a866a94315da1dfc038099ea2a
Krista Singleton <kris.singlet231@gmail.com>
2021-02-26 22:50:18 -0700

    Changed up the styles a bit

--- client/src/components/topBar.jsx
+++ client/src/components/topBar.jsx
@@ -11,7 +11,7 @@ const TopBar = ({ dispatch }) => {
   };
   return (
     <div className={classesMain.topBarBox}>
-      <h1>Krista Singleton: Portfolio</h1>
+      <h3>Krista Singleton: Portfolio</h3>
       <div className={classesMain.navBox}>
         <button type="button" className={classesMain.navBoxIcons} onClick={() => { changePage('home'); }}>HOME</button>
         <button type="button" className={classesMain.navBoxIcons} onClick={() => { changePage('appsPage'); }}>APPLICATIONS</button>
