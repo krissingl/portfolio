ebcf923636f546617edf87ef7a7f08c7f662712c
Krista Singleton <kris.singlet231@gmail.com>
2021-02-27 13:34:19 -0700

    Created random background image generator for first stab at business styling

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 import Main from './main.jsx';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      backgroundImgUrl: '',
     };
+    this.backgroundGen = this.backgroundGen.bind(this);
+  }
+
+  componentDidMount() {
+    this.backgroundGen();
+  }
+
+  backgroundGen() {
+    let url;
+    axios.get('http://www.splashbase.co/api/v1/images/random')
+      .then((response) => {
+        url = response.data.url;
+        this.setState({ backgroundImgUrl: url });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
