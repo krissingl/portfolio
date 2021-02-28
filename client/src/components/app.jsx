9d0747757eda142226f3d1e2ef8741d8b2214439
Krista Singleton <kris.singlet231@gmail.com>
2021-02-27 22:40:33 -0700

    Got rid of background generator stuff

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -6,30 +6,12 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      backgroundImgUrl: '',
     };
-    this.backgroundGen = this.backgroundGen.bind(this);
-  }
-
-  componentDidMount() {
-    this.backgroundGen();
-  }
-
-  backgroundGen() {
-    let url;
-    axios.get('http://www.splashbase.co/api/v1/images/random')
-      .then((response) => {
-        url = response.data.url;
-        this.setState({ backgroundImgUrl: url });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
   }
 
   render() {
     return (
-      <Main backgroundGen={this.backgroundGen} backgroundImgUrl={this.state.backgroundImgUrl} />
+      <Main />
     );
   }
 }
