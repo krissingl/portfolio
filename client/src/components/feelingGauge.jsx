c9c43307bc32f8ea3ed836eadd239201a284b06e
Krista Singleton <kris.singlet231@gmail.com>
2021-03-04 19:24:40 -0700

    Added feeling gauge component file

--- client/src/components/feelingGauge.jsx
+++ client/src/components/feelingGauge.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const FeelingGauge = () => {
-
+  console.log("feelingGuage");
+  return (
+    <div>
+      <h1>Ollo?</h1>
+    </div>
+  );
 };
 
 export default FeelingGauge;
